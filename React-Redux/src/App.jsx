302c197c2fe9488cee208c1d0c9b7ece5cdc105a
Raza-Anees <razaanees567@gmail.com>
2025-02-22 00:31:40 +0500

    using react-redux hook to manage the state and action of redux with useSelector and useDispatch

--- React-Redux/src/App.jsx
+++ React-Redux/src/App.jsx
@@ -2,11 +2,13 @@ import "./App.css";
 import Pizzabox from "./component/Pizzaboxx";
 import { Provider } from "react-redux";
 import store from "./component/redux/store";
+import HookContainer from "./component/HooksContainer";
 function App() {
   return (
     <>
       <Provider store={store}>
-        <Pizzabox />
+        {/* <Pizzabox /> */}
+        <HookContainer />
       </Provider>
     </>
   );
---
+++ React-Redux/src/component/HooksContainer.jsx
@@ -0,0 +1,20 @@
+import { useSelector, useDispatch } from "react-redux";
+import { orderPizza } from "./redux/pizza/PizzaActions";
+
+function HookContainer() {
+  // if using this hook we dont need to use the mapstatefuntion method
+  const PizzaBase = useSelector((state) => state.PizzaBase);
+  //to dispatch the action this hook have to reference to dispatch state of redux
+  const dispatch = useDispatch();
+
+  return (
+    <div className="container">
+      <h2 className="text">Number of pizzabase available- {PizzaBase}</h2>
+      <button className="btn" onClick={() => dispatch(orderPizza())}>
+        orderPizza
+      </button>
+    </div>
+  );
+}
+
+export default HookContainer;
