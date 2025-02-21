67163fa71473e5c97a19876f3bf79a283c8d9042
Raza-Anees <razaanees567@gmail.com>
2025-02-22 03:07:39 +0500

    configuration redux devtools extension

--- React-Redux/src/component/redux/store.js
+++ React-Redux/src/component/redux/store.js
@@ -1,11 +1,15 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 import { rootReducer } from "./rootReducer";
 
 const store = createStore(
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   rootReducer,
-  applyMiddleware(logger),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeWithDevTools(
+    applyMiddleware(logger)
+    // other store enhancers if any
+  )
 );
 export default store;
